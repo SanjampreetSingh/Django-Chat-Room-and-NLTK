4556ac4a9e85b55151c5473ecc1d9c5109ce8dd8
Sanjampreet Singh <sanjam531@gmail.com>
2019-07-26 23:25:05 +0530

    added mysql as DB

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -76,10 +76,14 @@ ASGI_APPLICATION = 'chatroom.routing.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'chatroom',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
 }
 
 
