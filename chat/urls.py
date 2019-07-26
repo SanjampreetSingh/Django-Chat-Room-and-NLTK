6a779830e80945c4d11edf5ca4f1002b36b49fd8
Sanjampreet Singh <sanjam531@gmail.com>
2019-07-26 19:00:57 +0530

    basic room template

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import index
+from django.urls import path, re_path
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
