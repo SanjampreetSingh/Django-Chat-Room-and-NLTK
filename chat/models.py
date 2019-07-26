2901ec51d1357becf48902bbed1a3d69a953e5e0
Sanjampreet Singh <sanjam531@gmail.com>
2019-07-26 22:10:04 +0530

    added models Room and Message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.TextField()
+    label = models.SlugField(unique=True)
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    handle = models.TextField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
